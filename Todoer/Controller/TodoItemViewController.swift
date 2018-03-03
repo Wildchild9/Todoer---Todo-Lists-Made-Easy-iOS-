4b8dcafe5a22361bf8503d7a11fe2d28cce29e53
Noah <noahleviwilder@gmail.com>
2018-03-03 12:19:40 -0500

    Made TableVIewController files and added protocol stubs and organized methods. All links have been setup.

---
+++ Todoer/Controller/TodoItemViewController.swift
@@ -0,0 +1,104 @@
+//
+//  TableViewController.swift
+//  Todoer
+//
+//  Created by Noah Wilder on 2018-03-03.
+//  Copyright © 2018 Noah Wilder. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+import ChameleonFramework
+import SwipeCellKit
+
+class CategoriesViewController: UITableViewController, SwipeTableViewCellDelegate {
+    
+    // MARK: - Class Variables & Values
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - View Methods
+    
+    // TODO: - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 80.00
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - TableView Datasource Methods
+    
+    //TODO: - Number of Sections
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    // TODO: - Number of Rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    // TODO: - Cell for Row at indexPath
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! SwipeTableViewCell
+        
+        // Configure the cell...
+        
+        cell.delegate = self
+        
+        return cell
+    }
+    
+    // TODO: - Did Select Row at indexPath
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    //MARK: - Swipe Cell Delegate Methods
+    
+    // TODO: - Edit Actions for Row at indexPath
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        
+        // Configure actions for swipe cell
+        
+        return nil // FIXME: Change to return to the desired swipe cell actions depending on the orientation
+    }
+    
+    
+    // TODO: - Edit Action Options for Row at indexPath
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
+        
+        let options = SwipeTableOptions()
+        // Configure action options for swipe cell
+        
+        return options
+    }
+    
+    
+    
+    
+    
+    
+    
+} // END OF CLASS //MARK: - End of Class
+
